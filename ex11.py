bdbfbcd19bbcbe63edd584bab54d683d5347997b
Rahul Saxena <rpalsaxena@gmail.com>
2016-07-20 12:48:14 +0530

    second commit

---
+++ ex11.py
@@ -0,0 +1,12 @@
+#Exercise 11
+
+print "How old are you?" ,
+age = int(raw_input())
+
+print "How tall are you?",
+height = raw_input()
+
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you are %r old, %r tall and %r heavy." %( age , height , weight)
