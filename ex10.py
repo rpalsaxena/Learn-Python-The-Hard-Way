639d62164eaca49ef5798e9112a28bd539604239
Rahul Saxena <rpalsaxena@gmail.com>
2016-07-20 12:18:26 +0530

    first commit

---
+++ ex10.py
@@ -0,0 +1,17 @@
+#Exercise 10
+
+tabby_cat = "\t T'm tabed in."
+persian_cat = "I'm split\n on a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat="""
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip \n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
